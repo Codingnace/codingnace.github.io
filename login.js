9177af7cc22e4dd78a6aecb38528d07065ea24ae
Codingnace <59060965+Codingnace@users.noreply.github.com>
2020-01-15 04:53:47 -0800

    Add files via upload

--- login.js
+++ login.js
@@ -1,1 +1,14 @@
-const 
+
+
+function check(){
+	var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+
+	if (username==null ||username=="") {alert("name must be filled");
+return false;
+}
+	if (password==null|| password=="") {alert("password must be filled");
+return false;
+}
+alert("login succeccfull");
+}
